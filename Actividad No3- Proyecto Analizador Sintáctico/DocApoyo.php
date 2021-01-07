fcdda8b4761c2f941eeb420ff9e2da846f6b9592
JoseChe36 <60972987+JoseChe36@users.noreply.github.com>
2021-01-07 15:34:17 -0600

    Add files via upload

---
+++ Actividad No3- Proyecto Analizador Sintáctico/DocApoyo.php
@@ -0,0 +1,26 @@
+<?php
+
+$var1 = 2;
+
+        echo "este es un cliclo";
+        echo "escribiendo";
+        $numero = 1;
+        $c = $b * 5;
+        $c = $b * 5;
+        $vari = 4.5 * (4.5*5);
+        $vari = $b * ($numero + 5);
+        $c = $b * 5;
+        
+        if ($a>=1 and $a<=100){
+            echo "a esta en el rango";
+            echo "MUY BIEN";
+        }else{
+            echo "a no esta en el rango";
+            echo "intentelo de nuevo";
+        }
+
+        if ($a == 2){
+            echo "este es el numero 2";
+        }
+    
+?>
